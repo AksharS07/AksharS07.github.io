f3e4bf0d08cd16f2708643f0e5951ee93e8a243d
AksharS07 <AksharS07@users.noreply.github.com>
2026-08-24 21:18:19 +0530

    Update Edge and Firefox store links to Glance for Web

--- js/scripts.js
+++ js/scripts.js
@@ -589,8 +589,8 @@ window.addEventListener("DOMContentLoaded", () => {
         "It supports Spotify Web, Apple Music, YouTube, & YouTube Music with a 60FPS Deep-Media Timer engine, " +
         "Universal Picture-in-Picture teleportation, and live time-synced lyrics.",
       tech: ["Vanilla JavaScript", "CSS", "Chrome Extension API", "Browser Modding", "LRCLib API", "Apple Music API"],
-      edgeUrl: "https://microsoftedge.microsoft.com/addons/detail/jhglafdjkeohejcgfdmcfhenniahjgpk",
-      firefoxUrl: "https://addons.mozilla.org/en-US/firefox/addon/dynamic-island-for-web/",
+      edgeUrl: "https://microsoftedge.microsoft.com/addons/detail/ihblpkbkbaeojengejgbnlcpfnaneegh",
+      firefoxUrl: "https://addons.mozilla.org/en-US/firefox/addon/glance-for-web/",
       features: [
         {
           icon: `<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M12 2a3 3 0 0 0-3 3v7a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3z"/><path d="M19 10v2a7 7 0 0 1-14 0v-2"/><line x1="12" y1="19" x2="12" y2="22"/><line x1="8" y1="22" x2="16" y2="22"/></svg>`,
